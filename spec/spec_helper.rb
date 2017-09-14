4739c0f9b072b60d05729b05e2f425580429e3a1
David Copeland <davetron5000@gmail.com>
2017-09-14 10:35:50 -0400

    add fittings.rb so Bundler defaults work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,16 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
-require 'mc-settings'
+require 'fittings'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-
+  if config.files_to_run.one?
+    config.default_formatter = "doc"
+  end
+  config.order = "random"
+  Kernel.srand config.seed
 end
