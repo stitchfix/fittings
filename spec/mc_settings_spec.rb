f0eef01057588bd735b799d29df77e4268a84f48
Sam Schenkman-Moore <sam.schenkman-moore@stitchfix.com>
2022-05-20 16:12:09 -0400

    Examples of access methods for Hashies

--- spec/mc_settings_spec.rb
+++ spec/mc_settings_spec.rb
@@ -11,6 +11,17 @@ describe Setting do
           :local => true)
     end
 
+    it 'should access via ... something' do
+      expect(Setting.two[:three]).to eq(5)
+      expect(Setting.two['three']).to eq(5)
+      expect(Setting.two.fetch(:three)).to eq(5)
+      expect(Setting.two.fetch('three')).to eq(5)
+      expect(Setting.two).to be_a_kind_of(Hashie::Mash)
+      expect(Setting.two.symbolize_keys).to eq(nil)
+      expect(Setting.two.keys).to eq(["three", "four"])
+      expect(Setting.two).to have_key(:three)
+    end
+
     it 'should return test specific values' do
       expect(subject.available_settings['one']).to eq("test")
       expect(subject.one).to eq("test")
